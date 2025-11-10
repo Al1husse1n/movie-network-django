af30741c5a61d5fbd10da4b442434be00a7a1c98
Ali Hussein <ah0979075074@gmail.com>
2025-11-10 13:39:45 +0300

    create views for login/signup

--- movie_network/myapp/views.py
+++ movie_network/myapp/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .Forms import *
+def login_view(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request,data=request.POST)
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request,user)
+            return redirect('home')
+    else:
+        login_form = LoginForm(request)
 
-# Create your views here.
+    return render(request, 'myapp/login.html', {"form":login_form})
+
+def signUp_view(request):
+    if request.method == "POST":
+        signup_form = SignUpForm(request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        signup_form = SignUpForm()
+    return render(request, 'myapp/signup.html', {"form":signup_form})
